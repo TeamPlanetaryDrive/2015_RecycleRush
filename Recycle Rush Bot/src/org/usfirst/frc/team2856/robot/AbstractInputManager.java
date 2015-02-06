fbbd73d6d4d1eb3dacccc4a9f5565540c554861c
mchopkins9 <mchopkins9@gmail.com>
2015-02-05 22:13:39 -0500

    Completed AbstractInputManager, no javadoc yet

--- Recycle Rush Bot/src/org/usfirst/frc/team2856/robot/AbstractInputManager.java
+++ Recycle Rush Bot/src/org/usfirst/frc/team2856/robot/AbstractInputManager.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team2856.robot;
 
+import java.util.LinkedList;
 import java.util.TreeMap;
 import java.util.function.Consumer;
 
@@ -7,7 +8,6 @@ import edu.wpi.first.wpilibj.Joystick;
 
 public abstract class AbstractInputManager {
 
-	// TODO: Complete value update and caching
 	public static final int
 			ATTACK3_AXIS_X = 0,
 			ATTACK3_AXIS_Y = 1,
@@ -34,72 +34,169 @@ public abstract class AbstractInputManager {
 			
 			XBOX_POV           = 0;
 			
-			
 	private    Joystick[] joysticks;
-	private       boolean currButton, oldButton;
-	private        double currAxis, oldAxis;
-	private           int currPOV, oldPOV;
+	private       boolean newButton;
+	private        double newAxis;
+	private           int newPOV;
 	
-	private TreeMap<Pair, Consumer<Boolean>> buttonMap;
-	private TreeMap<Pair, Consumer<Double>>  axisMap;
-	private TreeMap<Pair, Consumer<Integer>> povMap;
-	
-	protected boolean[][] buttons;
-	protected  double[][] axes;
-	protected     int[][] povs;
+	private TreeMap<JoyAddr, LinkedList<ValueHandler<Boolean>>> buttonMap;
+	private TreeMap<JoyAddr, LinkedList<ValueHandler<Double>>>  axisMap;
+	private TreeMap<JoyAddr, LinkedList<ValueHandler<Integer>>> povMap;
 	
 	public AbstractInputManager(Joystick... jsticks) {
 		joysticks = jsticks;
-		buttons = new boolean[joysticks.length][];
-		axes = new double[joysticks.length][];
-		povs = new int[joysticks.length][];
 		
-		for(int i = 0; i < joysticks.length; i++) {
-			buttons[i] = new boolean[joysticks[i].getButtonCount()];
-			axes[i] = new double[joysticks[i].getAxisCount()];
-			povs[i] = new int[joysticks[i].getPOVCount()];
-		}
-	}
-	
-	public void updateInputs() {
-		for(int i = 0; i < joysticks.length; i++) {
-			for(int j = 0; j < buttons[i].length; j++) {
-				buttons[i][j] = joysticks[i].getRawButton(j + 1);
-			}
-			
-			for(int j = 0; j < axes[i].length; j++) {
-				axes[i][j] = joysticks[i].getRawAxis(j);
-			}
-			
-			for(int j = 0; j < povs[i].length; j++) {
-				povs[i][j] = joysticks[i].getPOV(j);
-			}
-		}
+		buttonMap = new TreeMap<>();
+		axisMap = new TreeMap<>();
+		povMap = new TreeMap<>();
 	}
 	
 	public void update() {
-		for(Pair pair: buttonMap.keySet()) {
-			oldButton = joysticks[pair.ijoy].getRawButton(pair.ivalue);
-			
+		for(JoyAddr pair: buttonMap.keySet()) {
+			newButton = joysticks[pair.ijoy].getRawButton(pair.ivalue);
+			buttonMap.get(pair).forEach(handler -> {
+				if(!handler.curr.equals(newButton)) {
+					handler.curr = newButton;
+					handler.handler.accept(handler.curr);
+				}
+			});
+		}
+		
+		for(JoyAddr pair: axisMap.keySet()) {
+			newAxis = joysticks[pair.ijoy].getRawAxis(pair.ivalue);
+			axisMap.get(pair).forEach(handler -> {
+				if(!handler.curr.equals(newAxis)) {
+					handler.curr = newAxis;
+					handler.handler.accept(handler.curr);
+				}
+			});
+		}
+		
+		for(JoyAddr pair: povMap.keySet()) {
+			newPOV = joysticks[pair.ijoy].getPOV(pair.ivalue);
+			povMap.get(pair).forEach(handler -> {
+				if(!handler.curr.equals(newPOV)) {
+					handler.curr = newPOV;
+					handler.handler.accept(handler.curr);
+				}
+			});
 		}
 	}
 	
-	private static class Pair implements Comparable<Pair> {
+	public void addButtonAction(int joystick,
+			int button, Consumer<Boolean> handler) {
+		LinkedList<ValueHandler<Boolean>> list = new LinkedList<>();
+		list.addFirst(new ValueHandler<>(
+				joysticks[joystick].getRawButton(button), handler));
+		buttonMap.merge(new JoyAddr(joystick, button), list,
+				(v0, v1) -> {
+					v0.addFirst(v1.getFirst());
+					return v0;
+				});
+	}
+	
+	public void removeButtonAction(int joystick,
+			int button, Consumer<Boolean> handler) {
+		LinkedList<ValueHandler<Boolean>> list = new LinkedList<>();
+		list.addFirst(new ValueHandler<>(false, handler));
+		buttonMap.merge(new JoyAddr(joystick, button), list,
+				(v0, v1) -> {
+					v0.remove(v1.getFirst());
+					return v0.isEmpty() ? null : v0;
+				});
+	}
+	
+	public void addAxisAction(int joystick,
+			int axis, Consumer<Double> handler) {
+		LinkedList<ValueHandler<Double>> list = new LinkedList<>();
+		list.addFirst(new ValueHandler<>(
+				joysticks[joystick].getRawAxis(axis), handler));
+		axisMap.merge(new JoyAddr(joystick, axis), list,
+				(v0, v1) -> {
+					v0.addFirst(v1.getFirst());
+					return v0;
+				});
+	}
+	
+	public void removeAxisAction(int joystick,
+			int axis, Consumer<Double> handler) {
+		LinkedList<ValueHandler<Double>> list = new LinkedList<>();
+		list.addFirst(new ValueHandler<>(0.0, handler));
+		axisMap.merge(new JoyAddr(joystick, axis), list,
+				(v0, v1) -> {
+					v0.remove(v1.getFirst());
+					return v0.isEmpty() ? null : v0;
+				});
+	}
+	
+	public void addPOVAction(int joystick,
+			int pov, Consumer<Integer> handler) {
+		LinkedList<ValueHandler<Integer>> list = new LinkedList<>();
+		list.addFirst(new ValueHandler<>(
+				joysticks[joystick].getPOV(pov), handler));
+		povMap.merge(new JoyAddr(joystick, pov), list,
+				(v0, v1) -> {
+					v0.addFirst(v1.getFirst());
+					return v0;
+				});
+	}
+	
+	public void removePOVAction(int joystick,
+			int pov, Consumer<Integer> handler) {
+		LinkedList<ValueHandler<Integer>> list = new LinkedList<>();
+		list.addFirst(new ValueHandler<>(0, handler));
+		povMap.merge(new JoyAddr(joystick, pov), list,
+				(v0, v1) -> {
+					v0.remove(v1.getFirst());
+					return v0.isEmpty() ? null : v0;
+				});
+	}
+	
+	private static class JoyAddr implements Comparable<JoyAddr> {
 		
 		public final int ijoy;
 		public final int ivalue;
 		
-		public Pair(int ijoy, int ivalue) {
+		public JoyAddr(int ijoy, int ivalue) {
 			this.ijoy = ijoy;
 			this.ivalue = ivalue;
 		}
 		
 		@Override
-		public int compareTo(Pair o) {
-			return ijoy   != o.ijoy   ? (ijoy   > o.ijoy   ? 1 : -1)
+		public int compareTo(JoyAddr o) {
+			return o == null ? 1
+				 : ijoy   != o.ijoy   ? (ijoy   > o.ijoy   ? 1 : -1)
 				 : ivalue != o.ivalue ? (ivalue > o.ivalue ? 1 : -1)
 				 : 0;
 		}
 		
+		@Override
+		public boolean equals(Object o) {
+			return (o != null) && (o instanceof JoyAddr)
+					&& (compareTo((JoyAddr) o) == 0);
+		}
+		
 	}
+	
+	private static class ValueHandler<T> {
+		
+		public T curr;
+		public final Consumer<T> handler;
+		
+		public ValueHandler(T curr, Consumer<T> handler) {
+			this.curr = curr;
+			this.handler = handler;
+		}
+		
+		@Override
+		public boolean equals(Object o) {
+			if(o == null || !(o instanceof ValueHandler<?>)) {
+				return false;
+			}
+			
+			return (handler == ((ValueHandler<?>)o).handler);
+		}
+		
+	}
+	
 }
